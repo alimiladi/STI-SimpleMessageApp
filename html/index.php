0d66539d81e966242dba27043eb15ef212383a27
Ali Miladi <ali.miladi@heig-vd.ch>
2017-09-27 13:08:00 +0200

    Modified main script to redirect to the correct home page depending on the account's type

--- html/index.php
+++ html/index.php
@@ -1,25 +1,42 @@
 <?php
+	session_start();
+	if(!isset($_SESSION['login_user']))
+	{
+		header("location: login.php");
+	}
+	else
+	{
+		try{			
+			// Create (connect to) SQLite database in file
+			$dbconn = new PDO('sqlite:/var/www/databases/database.sqlite');
+			// Set errormode to exceptions
+			$dbconn->setAttribute(PDO::ATTR_ERRMODE, 
+			PDO::ERRMODE_EXCEPTION); 
 
-                      session_start();
+			$result = $dbconn->query("SELECT COUNT(*) as count FROM users WHERE username = '$username' AND admin = 1");
+			$count = $result->fetchColumn();
 
-                      if(!isset($_SESSION['login_user']))
-                      {
-                        header("location: login.php");
-                      }
-                      else
-                      {
-                        $username = $_SESSION['login_user'];
-                      }
+			if($count == 1)
+			{
+			  $_SESSION['admin'] = true;
+			  header("location: admin_home.php");
+			}
+			else{
+			   header("location: user_home.php");
+			}
+
+			// Close file db connection
+			$dbconn = null;
+		}
+
+		catch(PDOException $e) {
+			// Print PDOException message
+			echo $e->getMessage();
+		}
+
+		$username = $_SESSION['login_user'];
+		header("location: login.php");
+	}
 ?>
 
-<?php
-//redirect user to login if he is not connected otherwise redirect to its inbox
-if(!isset($_SESSION['login_user']))
-{
-  header("location: login.php");
-}
-else
-{
-  header("location: save_users.php");
-}
- ?>
+
