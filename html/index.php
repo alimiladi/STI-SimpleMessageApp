23a22e9b8520084e7a9af131b31e79ca02e26019
TCHENTE SIMO Dany Cleve <dany.tchentesimo@heig-vd.ch>
2017-09-27 13:20:30 +0200

    modif on populateBD index and process_login

--- html/index.php
+++ html/index.php
@@ -1,25 +1,14 @@
 <?php
 
-                      session_start();
+  session_start();
 
-                      if(!isset($_SESSION['login_user']))
-                      {
-                        header("location: login.php");
-                      }
-                      else
-                      {
-                        $username = $_SESSION['login_user'];
-                      }
+  if(!isset($_SESSION['login_user']))
+  {
+    header("location: login.php");
+  }
+  else
+  {
+    $username = $_SESSION['login_user'];
+  }
 ?>
 
-<?php
-//redirect user to login if he is not connected otherwise redirect to its inbox
-if(!isset($_SESSION['login_user']))
-{
-  header("location: login.php");
-}
-else
-{
-  header("location: save_users.php");
-}
- ?>
